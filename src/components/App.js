aebb7f8e4920e112e70c2569e9179b1f5e2b4f10
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-12-08 10:39:23 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [temp, setTemp] = useState({ temperature: 25, conditions: "Sunny" })
   return (
     <div>
         {/* Do not remove the main div */}
+        <p style={{color:temp.temperature>20?"red":"blue"}} >{temp.temperature}</p>
     </div>
   )
 }
